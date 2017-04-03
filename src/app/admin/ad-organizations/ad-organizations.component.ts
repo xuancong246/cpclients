232af46e4bf95d434d221e3ee4beabf6fea9cecd
Cong Pham <xuancong246@yahoo.com>
2017-04-03 22:08:17 +0700

    Read organization data from another source to our app

--- src/app/admin/ad-organizations/ad-organizations.component.ts
+++ src/app/admin/ad-organizations/ad-organizations.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AdOrganizationModel } from '../shared/models/ad-organization.model';
+import { AdOrganizationDataService } from '../shared/services/data/ad-organization-data.service';
+
 @Component({
-  selector: 'cps-ad-organizations',
-  templateUrl: './ad-organizations.component.html',
-  styleUrls: ['./ad-organizations.component.less']
+    selector: 'cps-ad-organizations',
+    templateUrl: './ad-organizations.component.html',
+    styleUrls: ['./ad-organizations.component.less']
 })
 export class AdOrganizationsComponent implements OnInit {
 
-  constructor() { }
+    constructor(private _adOrganizationDataService: AdOrganizationDataService) {
+    }
 
-  ngOnInit() {
-  }
+    organizations: AdOrganizationModel[] = [];
 
+    ngOnInit() {
+        let self = this;
+
+        this._adOrganizationDataService.getOrganizations()
+            .subscribe(
+            organizations => self.organizations = organizations);
+    }
 }
