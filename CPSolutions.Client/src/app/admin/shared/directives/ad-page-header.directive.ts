1d282b80a2be2bc52a3863593a09af4d31ee1240
Cong Pham <xuancong246@yahoo.com>
2017-04-23 18:10:09 +0700

    Create solution for both back-end side and client side. Create CPSolutions.Api project.

---
+++ CPSolutions.Client/src/app/admin/shared/directives/ad-page-header.directive.ts
@@ -0,0 +1,10 @@
+import { Directive, ElementRef, Renderer } from '@angular/core';
+
+@Directive({
+    selector: '[cpsAdPageHeader]'
+})
+export class AdPageHeaderDirective {
+    constructor(elementRef: ElementRef, renderer: Renderer) {
+        renderer.setElementClass(elementRef.nativeElement, 'ad-page__header', true);
+    }
+}
