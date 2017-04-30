02a846cb88cfc7cb733982ab66ebe67aefdf1bb8
Cong Pham <xuancong246@yahoo.com>
2017-04-30 16:46:25 +0700

    Restructure by using .Net Core for every back-end projects

---
+++ CPSolutions.Domain/Admin/Organization.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace CPSolutions.Domain.Admin
+{
+    public class Organization
+    {
+        [BsonElement("_id")]
+        public ObjectId Id { get; set; }
+        [BsonElement("name")]
+        public string Name { get; set; }
+        [BsonElement("address")]
+        public string Address { get; set; }
+        [BsonElement("description")]
+        public string Description { get; set; }
+    }
+}
